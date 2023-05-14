f6ce2b83242085d566f013ff0e76a999fe12cd42
gifplane <itact@hotmail.com>
2023-05-14 16:44:41 +0800

    Draggable Add Exit Event And Click Action

--- Assets/FernRender/FernNPRCore/StableDiffusionGraph/SDGraph/Editor/Manipulator/DraggableButton.cs
+++ Assets/FernRender/FernNPRCore/StableDiffusionGraph/SDGraph/Editor/Manipulator/DraggableButton.cs
@@ -1,3 +1,5 @@
+using System;
+using FernNPRCore.StableDiffusionGraph;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,7 +9,14 @@ public class DraggableButton : TextElement, IManipulator
     private Vector2 m_StartPosition;
     private VisualElement m_TargetElement;
     private float m_DownTime;
-    private float m_LongPressDuration = 1f;
+    private float m_LongPressDuration = 0.333f;
+
+    private Action<MouseUpEvent> m_OnClickAction;
+    public event Action<MouseUpEvent> OnClickAction
+    {
+        add => m_OnClickAction += value;
+        remove => m_OnClickAction -= value;
+    }
     
     public new static readonly string ussClassName = "unity-button";
 
@@ -16,18 +25,22 @@ public class DraggableButton : TextElement, IManipulator
         this.RegisterCallback<MouseMoveEvent>(OnMouseMove);
         this.RegisterCallback<MouseDownEvent>(OnMouseDown);
         this.RegisterCallback<MouseUpEvent>(OnMouseUp);
-        
-        this.AddToClassList(Button.ussClassName);
+        this.RegisterCallback<MouseLeaveEvent>(OnMouseLeave);
+        this.AddToClassList(ussClassName);
         this.focusable = true;
         this.tabIndex = 0;
     }
 
+    private void OnMouseLeave(MouseLeaveEvent evt)
+    {
+        m_Active = false;
+        m_DownTime = 0;
+    }
+
     public void OnMouseDown(MouseDownEvent evt)
     {
-
         if (evt.button == (int)MouseButton.LeftMouse)
         {
-            Debug.Log("Down");
             m_Active = true;
             m_StartPosition = evt.localMousePosition;
             m_TargetElement = evt.target as VisualElement;
@@ -40,7 +53,7 @@ public class DraggableButton : TextElement, IManipulator
     {
         if (m_Active && Time.realtimeSinceStartup - m_DownTime >= m_LongPressDuration)
         {
-            Debug.Log("Move");
+            SDUtil.Log("DraggableButton: Move");
             var delta = evt.localMousePosition - m_StartPosition;
             m_TargetElement.style.left = m_TargetElement.layout.x + delta.x;
             m_TargetElement.style.top = m_TargetElement.layout.y + delta.y;
@@ -50,12 +63,14 @@ public class DraggableButton : TextElement, IManipulator
 
     public void OnMouseUp(MouseUpEvent evt)
     {
-        if (evt.button == (int)MouseButton.LeftMouse)
+        if (m_Active && Time.realtimeSinceStartup - m_DownTime < m_LongPressDuration)
         {
-            Debug.Log("Up");
-            m_Active = false;
-            evt.StopPropagation();
+            SDUtil.Log("DraggableButton: Click Action");
+            m_OnClickAction?.Invoke(evt);
         }
+        m_Active = false;
+        m_DownTime = 0;
+        evt.StopPropagation();
     }
 
     public VisualElement target { get; set; }
